00d5f0448e0dd6bd5cf5c2a3c81c1237f7bf3b37
Your Name <you@example.com>
2025-02-28 11:13:46 +0500

    Fixed os import in config.py

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
-os.environ.get("7311810492:AAEyWpK0DI9OaKNmxdjpJJJeG1mICK8vpYc")
+import os
 
+BOT_TOKEN = os.environ.get("7311810492:AAEyWpK0DI9OaKNmxdjpJJJeG1mICK8vpYc")
 ADMIN_ID = 6564165978  # O‘zingning ID’ingni yoz
 ADMIN_USERNAME = "@Justin_Claberly"
 DATABASE_FILE = "teachers.db"
