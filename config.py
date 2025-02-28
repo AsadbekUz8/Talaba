0afd5df107b1022c2240eef0fd5f9a49f329a837
Your Name <you@example.com>
2025-02-28 11:54:18 +0500

    Updated config.py to support both local secrets and Render env vars

--- config.py
+++ config.py
@@ -1,7 +1,11 @@
 import os
 
-BOT_TOKEN = os.environ.get("7311810492:AAEyWpK0DI9OaKNmxdjpJJJeG1mICK8vpYc")
-ADMIN_ID = 6564165978  # O‘zingning ID’ingni yoz
+try:
+    from secrets import BOT_TOKEN  # Mahalliy sinov uchun secrets.py ishlatiladi
+except ImportError:
+    BOT_TOKEN = os.environ.get("BOT_TOKEN")  # Render’da Environment Variable’dan o‘qiladi
+
+ADMIN_ID = 6564165978
 ADMIN_USERNAME = "@Justin_Claberly"
 DATABASE_FILE = "teachers.db"
 DEFAULT_PHOTO = "https://suret.pics/uploads/posts/2023-09/1695449176_1-2.jpg"
